3166271c9eceeeff7a0dd1737a3033e63c430b18
Sean Feng <sean@seanfeng.dev>
2021-03-22 21:12:06 -0400

    Add missing file

---
+++ nvim/lua/plugins/nvim-bqf/init.lua
@@ -0,0 +1,9 @@
+require('bqf').setup({
+    auto_resize_height = false,
+    preview = {
+        win_height = 12,
+        win_vheight = 15,
+        delay_syntax = 80,
+        border_chars = {'┃', '┃', '━', '━', '┏', '┓', '┗', '┛', '█'}
+    },
+})
