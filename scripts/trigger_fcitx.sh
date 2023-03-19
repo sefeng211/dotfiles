f3429bb26c2a585d415a219aace8b157e8e1b8b8
Sean Feng <sefeng@mozilla.com>
2023-03-18 22:14:13 -0400

    No need for fcitx related scripts

--- scripts/trigger_fcitx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if fcitx5-remote | grep -q "2"; then
-    # echo "Need to send jj over"
-    # nvr --remote-send '<esc>'
-    for i in $(nvr --serverlist); do
-        nvr --remote-silent --remote-send '<esc>' --servername $i --nostart
-        echo "$i"
-    done
-fi
