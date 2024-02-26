81025e4d6266836cc9616a43a44d3fe7750dbe27
Sean Feng <sean@seanfeng.dev>
2024-02-26 08:49:04 -0500

    Add some Emacs config
    Explore how Emacs work

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,38 @@
+; Get the package.el going
+(require 'package)
+;; set package.el repositories
+(setq package-archives
+'(
+   ("org" . "https://orgmode.org/elpa/")
+   ("gnu" . "https://elpa.gnu.org/packages/")
+   ("melpa" . "https://melpa.org/packages/")
+))
+(package-initialize)
+;; update packages list if we are on a new install
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+; Use config.org as the config file for my emacs
+(org-babel-load-file
+  (expand-file-name
+   "config.org"
+   user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
