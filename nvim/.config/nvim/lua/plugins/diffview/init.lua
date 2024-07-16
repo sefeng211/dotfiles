d063fc5bba589d0648f31e7648870a0fb7780a6d
Sean Feng <sefeng@mozilla.com>
2024-07-16 16:09:31 -0400

    Add diffview

---
+++ nvim/.config/nvim/lua/plugins/diffview/init.lua
@@ -0,0 +1,9 @@
+require'diffview'.setup {
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ view = {
+   default = {
+     layout = "diff2_vertical",
+   }
+ },
+}
